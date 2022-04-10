5e73dfb0845fa4feebe7dd73933420e5c11e9516
Mahmoud-Sami <mahmoud-sami71@hotmail.com>
2022-04-11 03:41:09 +0400

    Create Project Structure

---
+++ BlogApp/Models/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlogApp.Models
+{
+    internal class BlogPost
+    {
+        public User User { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ BlogApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlogApp.Models
+{
+    internal class User
+    {
+        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        //-----------------------------------------
+        public int FullName { get; set; }
+    }
+}
